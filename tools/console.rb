938944c7b4014361c58a201bab9bf73da1fd90e5
Matthew <matthew@matthews-mbp.home>
2019-11-24 17:32:40 +0000

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -1,11 +1,26 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/bloodoath.rb'
+require_relative '../app/models/cult.rb'
+require_relative '../app/models/follower.rb'
 
-def reload
-  load 'config/environment.rb'
-end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+cult1 = Cult.new('Cult1', 'London', 2011, "Slogan1")
+cult2 = Cult.new('Cult2', 'Moscow', 2012, 'Slogan2')
+cult3 = Cult.new('Cult2', 'Moscow', 2012, 'Slogan2')
+
+follower1 = Follower.new('Name1', 22, 'Motto1')
+follower2 = Follower.new('Name2', 24, 'Motto2')
+
+follower3 = Follower.new('Name3', 26, 'Motto3')
+follower4 = Follower.new('Name4', 30, 'Motto4')
+
+
+bo1 = BloodOath.new(cult1, follower1, '23/04/1997')
+bo2 = BloodOath.new(cult1, follower2, '23/04/1997')
+
+bo3 = BloodOath.new(cult2, follower3, '23/04/1997')
+bo4 = BloodOath.new(cult2, follower4, '23/04/1997')
+
+
 
 binding.pry
 
